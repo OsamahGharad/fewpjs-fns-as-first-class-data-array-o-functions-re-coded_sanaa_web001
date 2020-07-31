734a7211233ee11859c83b1b0955c2bf8d0ebb04
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 14:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// When this program runs, it should print out:
+// 
+// ```
+// console.log("Wake Byron the poodle");
+// console.log("Leash Byron the poodle");
+// console.log("Walk to the park with Byron the poodle");
+// console.log("Throw the frisbee for Byron the poodle");
+// console.log("Walk home with Byron the poodle");
+// console.log("Unleash Byron the poodle");
+
 
 function wakeDog(dogName, dogBreed) {
 return `Wake ${dogName} the ${dogBreed}`;
@@ -20,3 +30,17 @@ return `Walk home with ${dogName} the ${dogBreed}`;
 function unleashDog(dogName, dogBreed) {
 return `Unleash ${dogName} the ${dogBreed}`;
 }
+
+let routine = [walkHome,leashDog,Walk,throwFrisbee,walkHome,unleashDog];
+
+function exerciseDog(dogName, dogBreed){
+  let result= [];
+  for(let i=0;i<=routine.length;i++){
+    
+  result.push(routine[i](dogName, dogBreed));
+  }
+  return 'result';
+}
+console.log(exerciseDog('lucy','bundle'));
+
+
