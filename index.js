becb1e875a539efe37f46baa54f8b0ff722d9a0f
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 15:34:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ let routine = [wakeDog ,leashDog ,walkToPark ,throwFrisbee
 function exerciseDog (dogName , dogBreed){
   console.log(routine.length);
   let result= [];
-  for(let i=0; i<=routine.length; i++){
+  for(let i=0; i<routine.length; i++){
    let string= routine[i](dogName,dogBreed);
   result.push(string);
   }
