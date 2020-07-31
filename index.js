5278b831fc4d082425e2ad4e9ed2e1f69e114519
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 15:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,42 +9,51 @@
 // console.log("Unleash Byron the poodle");
 
 
+
 function wakeDog(dogName, dogBreed) {
  let string =`Wake ${dogName} the ${dogBreed}`;
  console.log(string);
+ return string;
 
 }
 
 function leashDog(dogName, dogBreed) {
 let string = `Leash ${dogName} the ${dogBreed}`;
 console.log(string);
+return string;
 }
 
 function walkToPark(dogName, dogBreed) {
 let string = `Walk to the park with ${dogName} the ${dogBreed}`;
 console.log(string);
+return string;
 }
 
 function throwFrisbee(dogName, dogBreed) {
 let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
 console.log(string);
+return string;
 }
 function walkHome(dogName, dogBreed) {
 let string = `Walk home with ${dogName} the ${dogBreed}`;
 console.log(string);
+return string;
 }
 function unleashDog(dogName, dogBreed) {
 let string = `Unleash ${dogName} the ${dogBreed}`;
 console.log(string);
+return string;
+
 }
 
 let routine = [wakeDog ,leashDog ,walkToPark ,throwFrisbee
 ,walkHome,unleashDog] ;
 
 function exerciseDog(dogName, dogBreed){
+  console.log(routine.length);
   let result= [];
-  for(let i=0;i<=routine.length;i++){
-   let arrayO=routine[i](dogName, dogBreed);
+  for(let i=0; i<=routine.length; i++){
+   let arrayO= routine[i](dogName,dogBreed);
   result.push(arrayO);
   }
   return 'result';
