e64d1dbdedf63315b08193a80ed2dd19dbf62488
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 14:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ return `Wake ${dogName} the ${dogBreed}`;
 }
 
 function leashDog(dogName, dogBreed) {
-return `Wake ${dogName} the ${dogBreed}`;
+return `Leash ${dogName} the ${dogBreed}`;
 }
 
 function walkToPark(dogName, dogBreed) {
-return `Wake ${dogName} the ${dogBreed}`;
+return `Walk to the park ${dogName} the ${dogBreed}`;
 }
 
 function throwFrisbee(dogName, dogBreed) {
-return `Wake ${dogName} the ${dogBreed}`;
+return `Throw the frisbee ${dogName} the ${dogBreed}`;
 }
 function walkHome(dogName, dogBreed) {
-return `Wake ${dogName} the ${dogBreed}`;
+return `Walk home with ${dogName} the ${dogBreed}`;
 }
 function unleashDog(dogName, dogBreed) {
-return `Wake ${dogName} the ${dogBreed}`;
+return `Unleash ${dogName} the ${dogBreed}`;
 }
