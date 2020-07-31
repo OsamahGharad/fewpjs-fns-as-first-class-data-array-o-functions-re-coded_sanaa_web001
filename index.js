b6b6129540dc12a2a32afa8769d5139113fea433
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-31 13:59:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function wakeDog(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+
+function leashDog(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+
+function walkToPark(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+function walkHome(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+function unleashDog(dogName, dogBreed) {
+return `Wake ${dogName} the ${dogBreed}`;
+}
+
